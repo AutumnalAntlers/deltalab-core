e64647e848c91fd4d52393bcd2667ae9727e6179
adbenitez <asieldbenitez@gmail.com>
2022-01-03 03:00:26 -0500

    show incomming tgs as sticker

--- src/mimeparser.rs
+++ src/mimeparser.rs
@@ -440,11 +440,9 @@ impl MimeMessage {
             {
                 part.typ = Viewtype::Voice;
             }
-            if part.typ == Viewtype::Image || part.typ == Viewtype::Gif {
-                if let Some(value) = self.get_header(HeaderDef::ChatContent) {
-                    if value == "sticker" {
-                        part.typ = Viewtype::Sticker;
-                    }
+            if let Some(value) = self.get_header(HeaderDef::ChatContent) {
+                if value == "sticker" {
+                    part.typ = Viewtype::Sticker;
                 }
             }
             if part.typ == Viewtype::Audio
